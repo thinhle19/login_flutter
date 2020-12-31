543101483c415916827f63a08d1d0318e6a04110
Thinh Le <letienthinh0109@gmail.com>
2020-12-31 20:31:20 +0700

    add UserModel class for using provider

---
+++ lib/models/user_model.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/cupertino.dart';
+import 'package:login/models/user.dart';
+
+class UserModel extends ChangeNotifier {
+  List<User> users = [
+    User(
+      id: "1",
+      avatarUrl: "assets/avatar.jpg",
+      fullBodyImageUrl: "assets/full-body.png",
+      phone: "0936 572 310",
+      name: "Le Tien Thinh",
+      password: "@123",
+      userName: "@123",
+      description:
+          "The self-study lessons in this section are written and organised "
+          "according one two three four five six seven eight nine tento the levels of the Common European Framework of Reference for languages (CEFR). There are different types of texts and interactive exercises that practise the reading skills you need to do well in your studies, to get ahead at work and to communicate in English in your free time.",
+    ),
+    User(
+      id: "2",
+      avatarUrl: "assets/avatar.jpg",
+      fullBodyImageUrl: "assets/full-body.png",
+      phone: "0936 572 310",
+      name: "Le Tien Thinh",
+      password: "@123",
+      userName: "@123",
+      description:
+          "The self-study lessons in this section are written and organised "
+          "according one two three four five six seven eight nine tento the levels of the Common European Framework of Reference for languages (CEFR). There are different types of texts and interactive exercises that practise the reading skills you need to do well in your studies, to get ahead at work and to communicate in English in your free time.",
+    ),
+    User(
+      id: "3",
+      avatarUrl: "assets/avatar.jpg",
+      fullBodyImageUrl: "assets/full-body.png",
+      description:
+          "The self-study lessons in this section are written and organised "
+          "according one two three four five six seven eight nine tento the levels of the Common European Framework of Reference for languages (CEFR). There are different types of texts and interactive exercises that practise the reading skills you need to do well in your studies, to get ahead at work and to communicate in English in your free time.",
+      phone: "0936 572 310",
+      name: "Le Tien Thinh",
+      password: "@123",
+      userName: "@123",
+    ),
+  ];
+
+  void add(User user) {
+    users.add(user);
+    notifyListeners();
+  }
+}
